ceccbd760506fe1ba085746053c0e22d70e6a2e8
Tema Nwana <temanwana@Temas-MacBook-Pro.local>
2018-05-30 01:37:35 -0400

    added ActivityFeedServlet

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -1,7 +1,21 @@
 package codeu.controller;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**import codeu.model.data.ActivityFeed;*/
+
+/** Servlet class responsible for the activity feed. */
 public class ActivityFeedServlet extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getOutputStream().println("This is the Activity Feed");
+		
+	}
+
 
 }
