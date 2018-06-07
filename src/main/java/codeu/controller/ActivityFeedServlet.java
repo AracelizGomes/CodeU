d7cd05fa4b6eb7f97b7eb6a133f93ddd21fe097c
misstbn101 <35287487+misstbn101@users.noreply.github.com>
2018-06-06 22:58:52 -0400

    dealt with space before

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -6,8 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ActivityFeedServlet extends HttpServlet{
-	
+public class ActivityFeedServlet extends HttpServlet{	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.getOutputStream().println("This is the Activity Feed");	
