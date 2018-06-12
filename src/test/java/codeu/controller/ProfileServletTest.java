6890b064e08bf9d0e254302a6305d83bcdff588f
Araceliz Gomes (Local) <gomesaraceliz@gmail.com>
2018-06-12 14:22:14 -0400

    profile servlet tests

--- src/test/java/codeu/controller/ProfileServletTest.java
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -20,78 +20,45 @@ import org.mockito.Mockito;
 public class ProfileServletTest {
       private ProfileServlet profileServlet;
 	  private HttpServletRequest mockRequest;
-	  private RequestDispatcher mockRequestDispatcher;
+	  private HttpSession mockSession;
 	  private HttpServletResponse mockResponse;
+	  private RequestDispatcher mockRequestDispatcher;
+	  private UserStore mockUserStore;
 	  
 	  @Before
 	  public void setup() throws IOException {
 	    profileServlet = new ProfileServlet();
+	    
 	    mockRequest = Mockito.mock(HttpServletRequest.class);
+	    mockSession = Mockito.mock(HttpSession.class);
+	    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+	    
 	    mockResponse = Mockito.mock(HttpServletResponse.class);
 	    mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
 	    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
 	        .thenReturn(mockRequestDispatcher);
+	    
+	    mockUserStore = Mockito.mock(UserStore.class);
+	    profileServlet.setUserStore(mockUserStore);
 	  }
-
+	  
 	  
 	  @Test
 	  public void testDoGet_userValid() throws IOException, ServletException {
-		  User userMock = new User(
-				  UUID.randomUUID(), "Mock", "mockpassword", Instant.now());
-		  UserStore mockUserStore = Mockito.mock(UserStore.class);
-		  
-		  Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/mock");
-		  Mockito.when(mockUserStore.getUser("mock")).thenReturn(userMock);
+		  Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/test_username");
+		  Mockito.when(mockSession.getAttribute("user")).thenReturn("test_username");
+		  User fakeUser = new User(
+				  				UUID.randomUUID(), 
+				  				"test_username", 
+				  				"$2a$10$bBiLUAVmUFK6Iwg5rmpBUOIBW6rIMhU1eKfi3KR60V9UXaYTwPfHy", 
+				  				Instant.now());
+		  	  
+		 // Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/test_username");
+		  Mockito.when(mockUserStore.getUser("test_username")).thenReturn(fakeUser);
 		  
 		  profileServlet.setUserStore(mockUserStore);
 		  profileServlet.doGet(mockRequest, mockResponse);
+		  Mockito.verify(mockRequest).setAttribute("user", fakeUser);
 		  Mockito.verify(mockRequestDispatcher).forward(mockRequest,mockResponse);
 	  }
-	  
-	  @Test
-	  public void testDoGet_UserDoesNotExist() throws IOException, ServletException {
-		  
-		  UserStore mockUserStore = Mockito.mock(UserStore.class);
-		  
-		  Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/mock");
-		  Mockito.when(mockUserStore.getUser("Mock")).thenReturn(null);
-		  
-		  profileServlet.setUserStore(mockUserStore);
-		  profileServlet.doGet(mockRequest, mockResponse);
-		  Mockito.verify(mockRequestDispatcher).forward(mockRequest,mockResponse);
-	  }
-
-	  @Test
-	  public void testDoPost() throws IOException, ServletException {
-		  User userMock = new User(
-				  UUID.randomUUID(), "Mock", "mockpassword", Instant.now());
-		  UserStore mockUserStore = Mockito.mock(UserStore.class);
-		  
-		  Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/mock");
-		  Mockito.when(mockUserStore.getUser("Mock")).thenReturn(userMock);
-		  Mockito.when(mockRequest.getParameter("biography")).thenReturn("Mock Bio");
-		  
-		  profileServlet.setUserStore(mockUserStore);
-		  profileServlet.doGet(mockRequest, mockResponse);
-		  Assert.assertEquals("Mock Bio", userMock.getBiography());
-		  Mockito.verify(mockResponse).sendRedirect("/users/mock");
-	  }
-
-	  @Test
-	  public void testDoPost_UncleanedBio() throws IOException, ServletException {
-		  User userMock = new User(
-				  UUID.randomUUID(), "Mock", "mockpassword", Instant.now());
-		  UserStore mockUserStore = Mockito.mock(UserStore.class);
-		  
-		  Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/mock");
-		  Mockito.when(mockUserStore.getUser("Mock")).thenReturn(userMock);
-		  Mockito.when(mockRequest.getParameter("biography")).thenReturn("<h1> Mock's Bio </h1>");
-		  
-		  profileServlet.setUserStore(mockUserStore);
-		  profileServlet.doGet(mockRequest, mockResponse);
-		  Assert.assertEquals("Mock's Bio", userMock.getBiography());
-		  Mockito.verify(mockResponse).sendRedirect("/users/mock");
-	  }
-
-
 }
