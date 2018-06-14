3a102d36c06998721ca57cf34a4317ed1ef30d07
misstbn101 <35287487+misstbn101@users.noreply.github.com>
2018-06-14 01:20:04 -0400

    fixed indents

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -27,35 +27,33 @@ public class ActivityFeedServlet extends HttpServlet {
   * called when running in a server, not when running in a test.
   */
   @Override
-	public void init() throws ServletException {
-	  super.init();
-		setUserStore(UserStore.getInstance());
-		setConversationStore(ConversationStore.getInstance());
-	}
+  public void init() throws ServletException {
+    super.init();
+  setUserStore(UserStore.getInstance());
+  setConversationStore(ConversationStore.getInstance());
+  }
 
-	/**
-	 * Sets the UserStore used by this servlet. This function provides a common
-	 * setup method for use by the test framework or the servlet's init() function.
-	 */
-	void setUserStore(UserStore userStore) {
-		this.userStore = userStore;
-	}
+  /**
+  * Sets the UserStore used by this servlet. This function provides a common
+  * setup method for use by the test framework or the servlet's init() function.
+  */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
 
-	/**
-	 * Sets the ConversationStore used by this servlet. This function provides a
-	 * common setup method for use by the test framework or the servlet's init()
-	 * function.
-	 */
-	void setConversationStore(ConversationStore conversationStore) {
-		this.conversationStore = conversationStore;
-	}
+  /**
+  * Sets the ConversationStore used by this servlet. This function provides a
+  * common setup method for use by the test framework or the servlet's init()
+  * function.
+  */
+  void setConversationStore(ConversationStore conversationStore) {
+    this.conversationStore = conversationStore;
+  }
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-		List<Conversation> conversations = conversationStore.getAllConversations();
-		request.setAttribute("conversations", conversations);
-
-		request.getRequestDispatcher("/WEB-INF/view/ActivityFeed.jsp").forward(request, response);
-	}
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    List<Conversation> conversations = conversationStore.getAllConversations();
+    request.setAttribute("conversations", conversations);
+    request.getRequestDispatcher("/WEB-INF/view/ActivityFeed.jsp").forward(request, response);
+  }
 }
